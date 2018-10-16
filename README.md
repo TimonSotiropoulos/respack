22d0260d401c124a41508d2105e20a03cd175612
TimonSotiropoulos <timon.sotiropoulos@gmail.com>
2018-10-16 14:49:13 +1100

    Added respack working npm module hopefully to application

---
+++ README.md
@@ -0,0 +1,20 @@
+# respack
+Frontend <-> Backend API Library
+
+Number Formatter
+=========
+
+A small library that adds commas to numbers
+
+## Installation
+
+  `npm install @timoncs/respack`
+
+
+## Tests
+
+  `npm test`
+
+## Contributing
+
+In lieu of a formal style guide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code.
---
+++ src/respack.js
@@ -0,0 +1,147 @@
+'use strict';
+// *******************************************************
+// Respack
+// -------------------------------------------------------
+// The inner workings of the respack module
+// -------------------------------------------------------
+
+// *******************************************
+// Module Imports
+// -------------------------------------------
+import axios from 'axios';
+// --------------------------------
+
+// *******************************************
+// Implementation
+// -------------------------------------------
+const OKAY = 'OKAY';
+const FAILED = 'FAILED';
+const ERROR = 'ERROR';
+
+const GET = (url) => {
+    return axios(url, getRequestParams())
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+const POST = (url, body) => {
+    return axios(url, postRequestParams(body))
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+const sendOkay = (body) => {
+    return {
+        status: OKAY,
+        body: body || {}
+    };
+};
+
+const sendFailed = (body) => {
+    return {
+        status: FAILED,
+        body: body
+    };
+};
+
+const sendError = (body) => {
+    return {
+        status: ERROR,
+        body: body
+    };
+};
+
+const handleResponse = (res) => {
+    switch(res.data.status) {
+        case OKAY:
+            if (res.status === 200) {
+                return sendOkay(res.data.body);
+            } else {
+                return {status: ERROR, error: "Server Error..."};
+            }
+        case FAILED:
+            return sendFailed(res.data.body);
+        case ERROR:
+            return sendError(res.data.body);
+        default:
+            return sendError(res.data.body);
+    }
+}
+
+const handleError = (err) => {
+    if (err) {
+        return err;
+    } else {
+        return undefined;
+    }
+
+}
+
+/**
+ * @function getRequestParams
+ * Generates a standard get request parameters for a fetch request
+ * @returns {object} params - Basic params for a get request using fetch
+ */
+export const getRequestParams = () => {
+    const params = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        withCredentials: true,
+        data: undefined
+    }
+    return params;
+}
+
+/**
+ * @function postRequestParams
+ * Generates a standard post request parameters for a fetch request
+ * @params {object} body - JSON object to be passed onto the fetch API
+ * @returns {object} params - Basic params for a post request using fetch
+ */
+export const postRequestParams = (body) => {
+    const params = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        withCredentials: true,
+        data: body
+    }
+    return params;
+}
+
+// --------------------------------
+
+// *******************************************
+// Module Exports
+// -------------------------------------------
+const respack = {
+    STATUS: {
+        OKAY,
+        FAILED,
+        ERROR
+    },
+    TYPE: {
+        GET: "GET",
+        POST: "POST"
+    },
+    GET: GET,
+    POST: POST,
+    OKAY: sendOkay,
+    ERROR: sendError,
+    FAILED: sendFailed
+};
+export default respack;
+
+
+/**
+* @function test
+* Tests that all the application is working as expected
+* @returns none
+*/
+export const test = () => {
+    console.log("Test Successfull!");
+}
+// --------------------------------
